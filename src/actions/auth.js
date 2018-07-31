f9d7dadbbb8d4853e4682e74faecd7888927a8e1
Evan Norland <bladethetitan@gmail.com>
2018-07-31 17:37:27 +0000

    AUTHENTICATE USER reducer

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,15 +3,23 @@ import axios from 'axios';
 
 import { ROOT_URL } from '../config';
 
+import {
+    STORE_USER
+} from './types'
+
 export function signUp(fields, success) {
-    console.log(fields);
+    console.log(dispatch);
     return function (dispatch) {
-       // axios.post(`${ROOT_URL}/signUp`, fields);
-       // .then(response => {
-       //     
-       // })
-       // .catch(err => {
-       //     if(err) { console.log(err) }
-       // })
+        axios.post(`${ROOT_URL}/signUp`, fields);
+            .then(response => {
+                dispatch({
+                    type: STORE_USER,
+                    payload: response.data
+                })
+                success();
+            })
+            .catch(err => {
+                if(err) { console.log(err) }
+            })
     };
 }
