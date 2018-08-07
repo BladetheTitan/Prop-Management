197f1c934e3140bdb49d0949552a2244f721feec
Evan Norland <bladethetitan@gmail.com>
2018-08-07 15:32:13 +0000

    created and placed addnewsletter button on newsletter grid

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import Button from '../button';
 
 class NewsletterLatest extends Component {
+    
+    handleEdit = () => {
+        console.log('trying to handle edit');
+    }
+    
     render() {
+        const { title, imageUrl, body } = this.props;
         return (
             <div className='newsletter-latest'>
-                <h1 className='newsletter-latest__title'>Title goes here</h1>
-                <img className='newsletter-latest__image' src='http://via.placeholder.com/960x258'/>
+                <h1 className='newsletter-latest__title'>{title}</h1>
+                <img className='newsletter-latest__image' src={imageUrl}/>
+                <Button className='newsletter-latest__button'callback={() => this.handleEdit} icon='fas fa-pencil-alt'/>
                 <div className='newsletter-latest__body'>
-                    <p>Lorem Ipsum</p>
+                    <p>{body}</p>
                 </div>
             </div>
         );
